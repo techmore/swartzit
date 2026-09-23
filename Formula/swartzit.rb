9e0ac52ce107ebcd69eb599f474a91bb9c19671c
Sean Dolbec <sean@example.com>
2026-09-23 08:13:12 -0400

    Pin Homebrew formula to 0.1.1 release

--- Formula/swartzit.rb
+++ Formula/swartzit.rb
@@ -1,9 +1,9 @@
 class Swartzit < Formula
   desc "Self-hosted, pseudonymous discussion community"
   homepage "https://stoverparc.org"
-  version "0.1.0.20260923.12"
-  url "https://github.com/techmore/swartzit/archive/refs/tags/v0.1.0-20260923T12.tar.gz"
-  sha256 "2f8f65031ba775cc6281a1292620033db5b0a4f116441d9e39f19d3cddd733fa"
+  version "0.1.1.20260923.12"
+  url "https://github.com/techmore/swartzit/archive/refs/tags/v0.1.1-20260923T12.tar.gz"
+  sha256 "f88c2e3d689e323d00cfb8efa5a0e5d0510f1be54deedefe08223355e606a26f"
 
   depends_on "node" => :build
   depends_on "rust" => :build
