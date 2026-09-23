b9d8b2df79a767f66983923133d149500ccb4b4f
Sean Dolbec <sean@example.com>
2026-09-23 18:30:49 -0400

    Point Homebrew formula at pulse-aware update release

--- Formula/swartzit.rb
+++ Formula/swartzit.rb
@@ -1,9 +1,9 @@
 class Swartzit < Formula
   desc "Self-hosted, pseudonymous discussion community"
   homepage "https://stoverparc.org"
-  url "https://github.com/techmore/swartzit/archive/refs/tags/v0.1.19-20260923T18.tar.gz"
-  version "0.1.19.20260923.18"
-  sha256 "7e005f4fd400c61e2d93d3b58e098a19fc6d254eb2de00aa40a8668def6b2c47"
+  url "https://github.com/techmore/swartzit/archive/refs/tags/v0.1.20-20260923T18.tar.gz"
+  version "0.1.20.20260923.18"
+  sha256 "78c9a493209fc3090f8c2df620afa458798e7bdd007ae3c0e5d85643c440f6cd"
 
   depends_on "node" => :build
   depends_on "rust" => :build
